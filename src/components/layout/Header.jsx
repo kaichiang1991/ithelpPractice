80601dd691fa86188f29e86fbecc7ec5015927fe
Kai <addie60111@gmail.com>
2021-10-29 00:08:18 +0800

    完成基本 layout

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const StyledAppHeader = styled.header `
+`
+
+const Header = () => {
+  return (
+    <StyledAppHeader>
+      Header
+    </StyledAppHeader>
+  )
+}
+
+export default Header
