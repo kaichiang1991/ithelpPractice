f6f8a0c914b8bc5be76d9ae6f818bf8d297894ee
Kai <addie60111@gmail.com>
2021-10-29 16:13:23 +0800

    完成 footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,52 @@
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 
-const StyledAppFooter = styled.footer`
+const StyledAppFooter = styled.footer `
+  background-color: #343940;
 `
+
+// 內容的容器
+const StyledContentContainer = styled.div `
+  color: #fff;
+  padding: 48px 100px 32px;
+  font-size: 16px;
+`
+
+// 連結的容器
+const StyledLinkContainer = styled.span `
+  margin-right: 28px;
+
+  a{
+    color: #fff;
+    font-size: 16px;
+  }
+
+  a:hover{
+    color: #00A0EA;
+  }
+`
+
+// 連結的文字元件
+const LinkText = ({link, text}) => {
+  return (
+    <StyledLinkContainer>
+      <Link to={link}>{text}</Link>
+    </StyledLinkContainer>
+  )
+}
 
 const Footer = () => {
   return (
     <StyledAppFooter>
-      Footer
+      <StyledContentContainer>
+        <span>電週文化事業版權所有、轉載必究 | Copyright ©</span>
+        <LinkText link='/home' text='iThome'/>
+        <LinkText link='/commercial' text='刊登廣告'/>
+        <LinkText link='/authService' text='授權服務'/>
+        <LinkText link='/mail' text='服務信箱'/>
+        <LinkText link='/declaration' text='隱私權聲明與會員使用條款'/>
+        <LinkText link='/help' text='iT邦幫忙使用說明'/>
+      </StyledContentContainer>
     </StyledAppFooter>
   )
 }
